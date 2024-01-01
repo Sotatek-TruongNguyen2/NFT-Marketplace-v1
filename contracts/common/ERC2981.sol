92b3d02bc7332f8bb67fc1dd9b6aa13428e6609e
Maheswaranx15 <maheshwaran66648@gmail.com>
2024-01-01 12:27:47 +0530

    compiler updated

--- contracts/common/ERC2981.sol
+++ contracts/common/ERC2981.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity 0.8.21;
 
 import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 import "@openzeppelin/contracts/interfaces/IERC2981.sol";
--- hardhat.config.js
+++ hardhat.config.js
@@ -36,7 +36,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
   },
   
   solidity: {
-    version: "0.8.19",
+    version: "0.8.21",
     settings: {
       optimizer: {
         enabled: true,
