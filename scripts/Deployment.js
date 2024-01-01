c8faf79374b6943bcea659148eff1b435e2a25a6
Maheswaranx15 <maheshwaran66648@gmail.com>
2024-01-01 13:38:20 +0530

    depolyment abi updated

--- scripts/Deployment.js
+++ scripts/Deployment.js
@@ -4,6 +4,7 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 const hre = require("hardhat");
+const fs = require("fs");
 
 async function main() {
 
@@ -27,6 +28,13 @@ async function main() {
   await proxy.deployed();
   console.log("TransferProxy", proxy.address);
 
+  const ProxyContestdata = {
+    address: proxy.address,
+    abi: JSON.parse(proxy.interface.format('json'))
+  }
+
+  fs.writeFileSync('./Proxy.json', JSON.stringify(ProxyContestdata))
+
   // NFT721
   const erc721 = await ERC721.deploy(NFT721name,NFT721symbol,tokenURIPrefix);
   await erc721.deployed();
@@ -38,21 +46,52 @@ async function main() {
   await erc1155.deployed();
   console.log("ERC1155", nebula1155.address);
 
+  const ERC1155Contestdata = {
+    address: erc1155.address,
+    abi: JSON.parse(erc1155.interface.format('json'))
+  }
+
+  fs.writeFileSync('./ERC1155.json', JSON.stringify(ERC1155Contestdata))
+
+
   // trade
   const trade = await Trade.deploy(buyerFee,sellerFee,proxy.address);
   await trade.deployed();
   console.log("trade deployed to:", trade.address);
   await proxy.changeOperator(trade.address)
 
+  const TradeContestdata = {
+    address: trade.address,
+    abi: JSON.parse(trade.interface.format('json'))
+  }
+
+  fs.writeFileSync('./trade.json', JSON.stringify(TradeContestdata))
+
+
   // Factory721
   const factory721 = await Factory721.deploy()
   await factory721.deployed();
   console.log(`factory721`,factory721.address);
 
+  const Factory721Contestdata = {
+    address: factory721.address,
+    abi: JSON.parse(factory721.interface.format('json'))
+  }
+
+  fs.writeFileSync('./factory721.json', JSON.stringify(Factory721Contestdata))
+
   // Factory1155
   const factory1155 = await Factory1155.deploy()
   await factory1155.deployed()
   console.log(`factory1155:`,factory1155.address);
+
+
+  const Factory1155Contestdata = {
+    address: factory1155.address,
+    abi: JSON.parse(factory1155.interface.format('json'))
+  }
+
+  fs.writeFileSync('./factory1155.json', JSON.stringify(Factory1155Contestdata))
 
   let nft721Address = '/Deployed contract address/'
   let nft1155Address = '/Deployed contract address/'
