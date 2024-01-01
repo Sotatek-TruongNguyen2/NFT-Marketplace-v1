08a09604e1d9e9f506f2fb9676517388f1cb07c9
Maheswaranx15 <maheshwaran66648@gmail.com>
2024-01-01 11:03:00 +0530

    updated contract

--- contracts/common/ERC2981.sol
+++ contracts/common/ERC2981.sol
@@ -1,5 +1,5 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity 0.8.14;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
 
 import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 import "@openzeppelin/contracts/interfaces/IERC2981.sol";
