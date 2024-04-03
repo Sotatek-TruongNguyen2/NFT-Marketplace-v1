9647d8bbfc4643df1d27502bd4ae9ce7a8a7dec9
sotatek <sotatek@sotateks-MacBook-Pro-43.local>
2024-04-03 12:36:41 +0700

    Generalized hardhat

--- test/NFTMarketplace.js
+++ test/NFTMarketplace.js
@@ -1,14 +1,5 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
-const {
-  BN,           // Big Number support
-  constants,    // Common constants, like the zero address and largest integers
-  expectEvent,  // Assertions for emitted events
-  expectRevert, // Assertions for transactions that should fail
-} = require('@openzeppelin/test-helpers');
-require("@nomiclabs/hardhat-truffle5");
-
-
 
 describe("NFT Marketplace", function () {
   var proxyinstance;
